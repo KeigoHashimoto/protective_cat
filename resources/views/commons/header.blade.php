f244b75b47f4c4eaf9d338047550735d05a822b4
Keigo HAshimoto <kei.work.info@gmail.com>
2022-07-22 15:12:51 +0000

    design fix

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -9,20 +9,34 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         
-        <div class="collapse navbar-collapse mr-3" id="nav-bar">
+        <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {!! link_to_route('cats.create','猫を譲る',[],['class'=>'nav-link mr-5']) !!}
-                {!! link_to_route('cats.index','猫を探す',[],['class'=>'nav-link mr-5']) !!}
                 @if(Auth::check())
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{Auth::user()->name}}</a>
-                    <ul class="dropdown-menu dropdown-menu-right">
-                        <li class="dropdown-item">{!! link_to_route('welcome','MyProfile') !!}</li> 
-                        <li class="dropdown-item">{!! link_to_route('logout.get','ログアウト') !!}</li>
-                    </ul>
-                </li>
-               
+                    {!! link_to_route('cats.create','猫を譲る',[],['class'=>'nav-link mr-5']) !!}
+                    {!! link_to_route('cats.index','猫を探す',[],['class'=>'nav-link mr-5']) !!}
+                @else
+                    {!! link_to_route('signup.get','猫を譲る',[],['class'=>'nav-link mr-5']) !!}
+                    {!! link_to_route('guest.index','猫を探す',[],['class'=>'nav-link mr-5']) !!}
+                @endif
+                
+                @if(Auth::check())
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{Auth::user()->name}}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item">{!! link_to_route('welcome','MyProfile') !!}</li> 
+                            <li class="dropdown-item">{!! link_to_route('messages.get','Chatroom',[Auth::user()->id]) !!}</li>
+                            <li class="dropdown-item">{!! link_to_route('logout.get','ログアウト') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">guest</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item">{!! link_to_route('signup.get','新規登録') !!}</li> 
+                            <li class="dropdown-item">{!! link_to_route('login','ログイン') !!}</li>
+                        </ul>
+                    </li>
                 @endif
             </ul>
         </div>
--- resources/views/commons/profile.blade.php
+++ resources/views/commons/profile.blade.php
@@ -1,12 +1,20 @@
 <div class="container vw-80">
     <h2 claas="profile-title">Profile</h2>
     <div class="row profile">
-        <div class="col-sm-3"><img class=" profile-img" src="/*{{Auth::user()->user_image}}*/"></div>
-        <div class="col-sm-1"></div>
-        <div class="col-sm-8 profile-text">
-            <p>ニックネーム：{{$user->nickname}}</p>
-            <p>年齢：{{$user->age}}</p>
-            <p>{!!nl2br(e($user->comment))!!}</p>
+        <div class="col-md-3"><a href="{{route('user.show',[$user->id])}}"><img class=" profile-img" src="{{--{{Auth::user()->user_image}}--}}"></a></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-8">
+            <div class="profile-text">
+                <p>id：{{$user->id}}</p>
+                <p>ニックネーム：{{$user->nickname}}</p>
+                <p>年齢：{{$user->age}}</p>
+                <p>{!!nl2br(e($user->comment))!!}</p>
+            </div>
+            @if(Auth::id() != $user->id)
+                {!! link_to_route('messages.show','メッセージを送る',[$user->id],['class'=>'btn mt-3 form-control']) !!}
+            @else
+                {!! link_to_route('messages.get','メッセージボード',[$user->id],['class'=>'btn mt-3 form-control']) !!}
+            @endif
         </div>
     </div>
 </div>
